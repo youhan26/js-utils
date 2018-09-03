b6e8c9060db975e1d529c671b9ff9c6cd4ecc5e6
YouHan26 <youhan26@gmail.com>
2018-09-03 09:43:39 +0800

    version 2.0 init

--- src/common.js
+++ src/common.js
@@ -1,11 +1,52 @@
+import is from './is.js';
 
-const emptyfunc = () => {
-};
+const {isObject, isFunction, isDate} = is;
 
-const noop = function () {
-};
+function extend(dst) {
+  return baseExtend(dst, slice.call(arguments, 1), false);
+}
+
+function baseExtend(dst, objs, deep) {
+  const h = dst.$$hashKey;
+  
+  for (let i = 0, ii = objs.length; i < ii; ++i) {
+    const obj = objs[i];
+    if (!isObject(obj) && !isFunction(obj)) continue;
+    const keys = Object.keys(obj);
+    for (let j = 0, jj = keys.length; j < jj; j++) {
+      const key = keys[j];
+      const src = obj[key];
+      
+      if (deep && isObject(src)) {
+        if (isDate(src)) {
+          dst[key] = new Date(src.valueOf());
+        } else {
+          if (!isObject(dst[key])) dst[key] = Array.isArray(src) ? [] : {};
+          baseExtend(dst[key], [src], true);
+        }
+      } else {
+        dst[key] = src;
+      }
+    }
+  }
+  
+  setHashKey(dst, h);
+  return dst;
+}
+
+function setHashKey(obj, h) {
+  if (h) {
+    obj.$$hashKey = h;
+  } else {
+    delete obj.$$hashKey;
+  }
+}
+
+function noop() {
+
+}
 
 export default {
-  emptyfunc,
-  noop
+  noop,
+  extend,
 };
