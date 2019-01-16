16bc4d026783f3d73dfce2ff69078646141700fd
youhan26 <youhan26@gmail.com>
2019-01-16 14:46:17 +0800

    add setImmediate ployfill

--- src/setImmediatePloyfill.js
+++ src/setImmediatePloyfill.js
@@ -1,19 +1,27 @@
 const setImmediatePloyfill = (function () {
 
   // node environment or msie
-  if (typeof setImmediate !== "undefined") {
+  if (typeof setImmediate !== 'undefined') {
     return setImmediate;
   }
 
-  const queue = [];
+  if (typeof Promise !== 'undefined') {
+    const p = Promise.resolve();
+    return function (fn) {
+      p.then(fn);
+    };
+  }
+
+  let queue = [];
 
   // if support MutationObserver
-  if (typeof MutationObserver !== "undefined") {
+  if (typeof MutationObserver !== 'undefined') {
     const dd = document.createElement("div");
     const mo = new MutationObserver(function () {
       queue.forEach(function (item) {
         item && item();
       });
+      queue = [];
     });
     mo.observe(dd, {attributes: true});
 
