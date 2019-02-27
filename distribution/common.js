9a10518b7d35238b82d430333a36b538033a55c6
Han You <youhan26@gmail.com>
2019-02-27 14:52:23 +0800

    add def.js

--- distribution/common.js
+++ distribution/common.js
@@ -1,14 +1,63 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var emptyfunc = function emptyfunc() {};
+var _is = require('./is.js');
 
-var noop = function noop() {};
+var _is2 = _interopRequireDefault(_is);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var isObject = _is2.default.isObject,
+    isFunction = _is2.default.isFunction,
+    isDate = _is2.default.isDate;
+
+
+function extend(dst) {
+  return baseExtend(dst, slice.call(arguments, 1), false);
+}
+
+function baseExtend(dst, objs, deep) {
+  var h = dst.$$hashKey;
+
+  for (var i = 0, ii = objs.length; i < ii; ++i) {
+    var obj = objs[i];
+    if (!isObject(obj) && !isFunction(obj)) continue;
+    var keys = Object.keys(obj);
+    for (var j = 0, jj = keys.length; j < jj; j++) {
+      var key = keys[j];
+      var src = obj[key];
+
+      if (deep && isObject(src)) {
+        if (isDate(src)) {
+          dst[key] = new Date(src.valueOf());
+        } else {
+          if (!isObject(dst[key])) dst[key] = Array.isArray(src) ? [] : {};
+          baseExtend(dst[key], [src], true);
+        }
+      } else {
+        dst[key] = src;
+      }
+    }
+  }
+
+  setHashKey(dst, h);
+  return dst;
+}
+
+function setHashKey(obj, h) {
+  if (h) {
+    obj.$$hashKey = h;
+  } else {
+    delete obj.$$hashKey;
+  }
+}
+
+function noop() {}
 
 exports.default = {
-  emptyfunc: emptyfunc,
-  noop: noop
+  noop: noop,
+  extend: extend
 };
