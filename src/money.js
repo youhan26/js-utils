ff5eeca887c7df8cb7b4aaea1052ac4bf9440f6a
Han You <youhan26@gmail.com>
2019-04-10 22:19:40 +0800

    add moeny format

--- src/money.js
+++ src/money.js
@@ -2,19 +2,29 @@
  * format price
  */
 const formatMoney = (money) => {
-  if (!money) {
-    return '0';
-  }
-  
-  if (Number.isInteger(parseFloat(money))) {
-    return parseInt(money);
-  }
-  return parseFloat(money).toFixed(2);
+	if (!money) {
+		return '0';
+	}
+
+	if (Number.isInteger(parseFloat(money))) {
+		return parseInt(money);
+	}
+	return parseFloat(money).toFixed(2);
+};
+
+/**
+ * formatMoneyStr: change 1000 to 1,000
+ * @param num
+ * @returns {string}
+ */
+const formatMoneyStr = function (num) {
+	return String(num).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 
 const money = {
-  fix2End: formatMoney
+	fix2End: formatMoney,
+	formatMoneyStr
 };
 
 export default money;
