e9ab17aa99aa0021a4023d82738c3a5748fb8517
Han You <youhan26@gmail.com>
2019-05-14 21:38:20 +0800

    add isEmpty

--- src/is.js
+++ src/is.js
@@ -1,69 +1,77 @@
-
 function isObject(arg) {
-  return arg !== null && typeof arg === 'object';
+	return arg !== null && typeof arg === 'object';
 }
 
 // notice: window.isNaN() === true
 function isNaN(obj) {
-  return obj !== obj;
+	return obj !== obj;
+}
+
+/**
+ * null / void 0
+ * @param s
+ * @returns {boolean}
+ */
+function isEmpty(s) {
+	return s == null;
 }
 
 function isNull(obj) {
-  return obj === null;
+	return obj === null;
 }
 
 function isUndefined(obj) {
-  return obj === void 0;
+	return obj === void 0;
 }
 
 function isFunction(arg) {
-  return typeof arg === 'function';
+	return typeof arg === 'function';
 }
 
 function isDate(value) {
-  return toString.call(value) === '[object Date]';
+	return toString.call(value) === '[object Date]';
 }
 
 function isNumber(value) {
-  return typeof value === 'number';
+	return typeof value === 'number';
 }
 
 function isWindow(obj) {
-  return obj && obj.window === obj;
+	return obj && obj.window === obj;
 }
 
 function isBlob(obj) {
-  return toString.call(obj) === '[object Blob]';
+	return toString.call(obj) === '[object Blob]';
 }
 
 function isBoolean(value) {
-  return typeof value === 'boolean';
+	return typeof value === 'boolean';
 }
 
 function isPromiseLike(obj) {
-  return obj && isFunction(obj.then);
+	return obj && isFunction(obj.then);
 }
 
 function isString(value) {
-  return typeof value === 'string';
+	return typeof value === 'string';
 }
 
 function isPlainObject(value) {
-  return value !== null && typeof value === 'object' && !Object.getPrototypeOf(value);
+	return value !== null && typeof value === 'object' && !Object.getPrototypeOf(value);
 }
 
 export default {
-  isNaN,
-  isNull,
-  isUndefined,
-  isObject,
-  isFunction,
-  isDate,
-  isNumber,
-  isBlob,
-  isBoolean,
-  isPromiseLike,
-  isString,
-  isPlainObject,
-  isWindow
+	isNaN,
+	isNull,
+	isUndefined,
+	isObject,
+	isFunction,
+	isDate,
+	isNumber,
+	isBlob,
+	isBoolean,
+	isPromiseLike,
+	isString,
+	isPlainObject,
+	isWindow
 };
